d7baac369cc2fddcbf9f6189422a2c129ba50f5a
Mimo <mimoriam420@gmail.com>
2023-08-12 15:16:53 +0500

    Commit #5 - Created a Modal but require a state management library to manage open/close states

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
+'use client';
+
 import { SignOutButton, UserButton } from '@clerk/nextjs';
+import { Modal } from '@/components/ui/modal';
 
 export default function SetupPage() {
   return (
     <main>
       <div>Hello World 2!</div>
       <UserButton afterSignOutUrl="/" />
+      <Modal title="Testing" description="Wohoo!" isOpen onClose={() => {}}>
+        CHILDREN
+      </Modal>
     </main>
   );
 }
