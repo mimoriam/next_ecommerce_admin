c633ec9c0a3d4dc087861dc44be841e1d66b9d95
Mimo <mimoriam420@gmail.com>
2023-08-12 15:32:08 +0500

    Commit #6 - Modals created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,15 +2,23 @@
 
 import { SignOutButton, UserButton } from '@clerk/nextjs';
 import { Modal } from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <main>
       <div>Hello World 2!</div>
       <UserButton afterSignOutUrl="/" />
-      <Modal title="Testing" description="Wohoo!" isOpen onClose={() => {}}>
-        CHILDREN
-      </Modal>
     </main>
   );
 }
